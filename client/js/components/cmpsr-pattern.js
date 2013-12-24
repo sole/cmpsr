1fbc89beaaa11f8b35df74d17cb926bf3b131f57
sole <listas@soledadpenades.com>
2013-12-24 18:41:03 +0100

    client: cmpsr-pattern - add/remove tracks (as children)

--- client/js/components/cmpsr-pattern.js
+++ client/js/components/cmpsr-pattern.js
@@ -4,7 +4,10 @@ xtag.register('cmpsr-pattern', {
 	// setData( <massive object with data> ) or individual per cell methods? (or both)
 	lifecycle: {
 		created: function() {
-			this.innerHTML = 'a pattern';
+			this.tracks = 8;
+			this.innerHTML = '';
+			
+			this.setNumTracks(this.tracks);
 		}
 	},
 	accessors: {
@@ -13,6 +16,35 @@ xtag.register('cmpsr-pattern', {
 			set: function(v) {
 				console.log('set lines', v);
 				// TODO propagate
+			}
+		},
+		tracks: {
+			attribute: { },
+			set: function(v) {
+				console.log('set tracks', v);
+				// TODO propagate
+				this.setNumTracks(parseInt(v, 10));
+			}
+		}
+	},
+	methods: {
+		setNumTracks: function(v) {
+			var trackElements = this.querySelectorAll('cmpsr-pattern-track');
+			var i;
+			var diff = trackElements.length - v;
+
+			if(diff < 0) {
+				// add new tracks
+				for(i = diff; i < 0; i++) {
+					var el = document.createElement('cmpsr-pattern-track');
+					this.appendChild(el);
+					// TODO set track num lines
+				}
+			} else {
+				// remove tracks
+				for(i = 0; i < diff; i++) {
+					this.removeChild(this.lastChild);
+				}
 			}
 		}
 	}
--- tests/test-cmpsr-pattern.js
+++ tests/test-cmpsr-pattern.js
@@ -5,15 +5,41 @@ function testCmpsrPattern() {
 	test('declarative init', function() {
 		var el = document.querySelector('cmpsr-pattern');
 		ok(el !== null);
+		equal(el.querySelectorAll('cmpsr-pattern-track').length, 8);
 	});
 
+	// Make sure attributes are set when using accessors
 	test('attributes', function() {
 		var el = document.createElement('cmpsr-pattern');
-		var numLines = 32;
-		el.lines = numLines;
-		equal(el.lines, numLines);
+		var pairs = [
+			[ 'lines', 32 ],
+			[ 'tracks', 8 ]
+		];
+
+		pairs.forEach(function(pair) {
+
+			var attribute = pair[0];
+			var expectedValue = pair[1];
+			
+			el[attribute] = expectedValue;
+
+			equal(el[attribute], expectedValue);
+			equal(el.getAttribute(attribute), expectedValue);
+
+		});
 
 		document.body.appendChild(el);
+	});
+
+	test('children == tracks', function() {
+		var el = document.createElement('cmpsr-pattern');
+
+		el.tracks = 16;
+		equal(el.querySelectorAll('cmpsr-pattern-track').length, 16);
+
+		el.tracks = 8;
+		equal(el.querySelectorAll('cmpsr-pattern-track').length, 8);
+
 	});
 
 }
